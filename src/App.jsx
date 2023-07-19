4393d22b8ea31925d10fafd6f8fbabe344fe6dc2
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-07-19 20:15:29 +0700

    Create skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
+import { Skeleton } from "./components/PizzaBlock/Skeleton";
 import Sort from "./components/Sort";
 // import pizzas from "./assets/pizzas.json";
 import "./scss/app.scss";
@@ -8,11 +9,15 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://64b6a381df0839c97e15e8f4.mockapi.io/items")
       .then((res) => res.json())
-      .then((res) => setPizzas(res));
+      .then((arr) => {
+        setPizzas(arr);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -26,16 +31,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
-              <PizzaBlock
-                key={pizza.id}
-                title={pizza.title}
-                price={pizza.price}
-                image={pizza.imageUrl}
-                sizes={pizza.sizes}
-                types={pizza.types}
-              />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+              : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
           </div>
         </div>
       </div>
--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { act } from "react-dom/test-utils";
 
-const PizzaBlock = ({ title, price, image, sizes, types }) => {
+const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
   const [activeSize, setActiveSize] = useState(0);
   const [activeType, setActiveType] = useState(0);
 
@@ -9,7 +9,7 @@ const PizzaBlock = ({ title, price, image, sizes, types }) => {
 
   return (
     <div className="pizza-block">
-      <img className="pizza-block__image" src={image} alt="Pizza" />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
